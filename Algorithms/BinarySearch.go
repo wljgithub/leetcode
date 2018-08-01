a73a254254079082d3654e3a6cc6180f182be2af
jack <wulingjian123@gmail.com>
2018-08-01 09:08:02 +0800

    commit change avoid a new branch

--- Algorithms/BinarySearch.go
+++ Algorithms/BinarySearch.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-)
+	)
 
+// 二分查找正常实现
 func BinarySearch(arr []int, key int) (index int) {
 	l, h := 0, len(arr)-1
 	//fmt.Println(l, h,mid)
@@ -22,7 +23,22 @@ func BinarySearch(arr []int, key int) (index int) {
 	return -1
 }
 
+// 二分查找的变种 --在包含重复的数组中，寻找最左边的元素
+func BinarySearchA(arr []int,key int)(index int)  {
+	l,h:=0,len(arr)
+	for l<h{
+		mid :=l+(h-l)/2
+		if arr[mid]>=key{
+			h=mid
+		}else{
+			l = mid+1
+		}
+	}
+	return l
+}
+
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(BinarySearch(arr, 8))
+	fmt.Println(BinarySearch(arr, 10))
+
 }
