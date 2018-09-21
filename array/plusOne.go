d83b124c35becb66d9f723192d2ae97af2bcb817
jack <wulingjian123@gmail.com>
2018-09-21 10:02:29 +0800

    add another solve method

--- array/plusOne.go
+++ array/plusOne.go
@@ -14,13 +14,42 @@ func plusOne(arr []int) []int {
 	//+1
 	sum += 1
 	//将数字还原为数组
+
+	lenght:=1
+	temp:=sum
+	for temp>=9 {
+		lenght+=1
+		temp/=10
+	}
+	l := make([]int,lenght)
+
+	for i:=lenght-1;i>=0 ;i--  {
+		l[i] = sum%10
+		sum /= 10
+	}
+
+	return l
+}
+func plusOne2(arr []int) []int {
+	// 把数组拼成数字
+	sum := 0
+	count := 0
+	for i := len(arr) - 1; i >= 0; i-- {
+		sum += arr[i] * power(10, count)
+		count += 1
+	}
+	fmt.Println("sum:", sum)
+	//+1
+	sum += 1
+
+	//将数字还原为数组
 	var l []int
 	for sum>=1{
 		l = append(l,sum%10)
 		sum/=10
 	}
 
-	// 数组反转
+	//数组反转
 	left,right:=0,len(l)-1
 	for left<right{
 		l[left],l[right]=l[right],l[left]
@@ -61,8 +90,9 @@ func power2(num, power int) int {
 //
 //}
 func main() {
-	i := []int{1,0}
+	i := []int{3,2,1}
 	fmt.Println(plusOne(i))
+	fmt.Println(plusOne2(i))
 
 
 }
